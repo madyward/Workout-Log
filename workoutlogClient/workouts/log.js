57242ec811815424cdca0bebbc5eef8a8e9709e9
Madyson Ward <MadysonWard94@gmail.com>
2017-09-05 14:07:52 -0400

    Added log.js file to /workoutClient/workouts, modified index.html to add log tab

---
+++ workoutlogClient/workouts/log.js
@@ -0,0 +1,68 @@
+$(function(){
+	$.extend(WorkoutLog, {
+		log: {
+			workouts: [],
+
+			setDefinitions: function(){
+				var defs = WorkoutLog.definition.userDefinitions;
+				var len = defs.length
+				var opts;
+				for (var i = 0; i < len; i++){
+					opts += "<option value ='" + defs[i].id + "'>" + defs[i].description + "</option>"''
+				}
+				$("#log-definition").children().remove();
+				$("#log-definition").append(opts);
+			},
+			setHistory: function(){
+				var history = WorkoutLog.log.workouts;
+				var len = history.length;
+				var lis = "";
+				for (var i = 0; i < len; i++){
+					lis += "<li class='list-group-item'>" + history[i].def + "-" + history[i].result + "</li>";
+				}
+				$("#history-list").children().remove();
+				$("#history-list").append(lis);
+			},
+			create: function(){
+				var itsLog = {
+					desc: $("#log-description").val(),
+					result: $ß("#log-result").val(),
+					def: $("#log-definition option:selected").text()
+				};
+				var postData = {log: itsLog};
+				var logger = $.ajax({
+					type: "POST",
+					url: WorkoutLog.API_BASE + "log",
+					dataType: JSON.stringify(postData),
+					contentType: "application/json"
+				});
+				logger.done(function(data){
+					WorkoutLog.log.workouts.push(data);
+				})
+			},
+			fetchAll: function(){
+				var fetchDefs = $.ajax({
+					type: "GET",
+					url: WorkoutLog.API_BASE + "log",
+					headers: {
+						"authorization":
+							window.localStorage.getItem("sessionToken")
+					}
+				})
+				.done(function(data){
+					WorkoutLog.log.workouts = data;
+				})
+				.fail(function(err){
+					console.log(err);
+				});
+			}
+		}
+	})
+
+	//click the button and create a log entry
+	$("#log-save").on("click", WorkoutLog.log.create);
+
+	if (window.localStorage.getItem("sessionToken")){
+		WorkoutLog.log.fetchAll();
+	}
+});
